05166bfa74d885090681690ba1e6a69911b67d4d
effbiae <effbiae@gmail.com>
2025-06-21 13:08:50 +1000

    ff/z works

--- k.py
+++ k.py
@@ -1,5 +1,11 @@
 from p import p,P;import sys
+def k2(x,y,z):
+ if x=='+':return y+z
+ if x=='>':return y>z
+def k1(x,y):
+ return None
 def ev(x):
+ if x is None:return x
  try:return int(x)
  except:pass
  if x[0]=='!':return tuple(range(ev(x[1])))
@@ -10,19 +16,28 @@ def ev(x):
   if x[0]in"\\/'":return ev(x[1])+x[0]
  if x in P:return x
  print(x);assert(0)
-def accu_(f,y,z):
- if not len(z):return()
- r0=f(y,z[0]);return (r0,)+(accu_(f,r0,z[1:])if 1<len(z)else())
-def accum1(f,y,z):return accu_(f,y,z)
-def accum0(f,y,z):return()if not len(z)else(z[0],)+accu_(f,z[0],z[1:])
-def scan(x,y,z):
- if x=='+':return(accum1,accum0)[y is None](lambda x,y:x+y,ev(y)if y else y,z)
-def over(x,y,z):return scan(x,y,z)[-1]
+nn=lambda x:x is not None
+def ar(x):
+ if type(x)==str:return (2,1)[':'in x]
+ if type(x)==tuple:return 1
+def ap(x,y):
+ if type(x)==str:return (k2,k1)[':'in x](x[0],*y)
+ if type(x)==tuple:
+  if len(x)==3 and x[2]==():return ap(x[0],(ev(x[1]),y[0]))
+def oid(f):
+ if f=='+':return 0
+def acb(f,y,z):
+ t=z;r=()
+ while ap(y,(t,)):r+=(t,);t=ap(f,(t,))
+ return r
+def acc(f,y,z):r=();t=ev(y);t=t if nn(t)else oid(f);[(t:=ap(f,(t,_)),r:=r+(t,))for _ in z];return r
+def scan(x,y,z):return (acc,acb)[1==ar(x)](x,y,z)
+def over(x,y,z):x=scan(x,y,z);return x[-1]if x else x
 ad={'\\':scan,'/':over,"'":None}
 def adv(x,y,z):return ad[x[0]](x[1],y,z)
 def t():
  def eq(x,y):z=ev(p(x));(print(x,'was',z,'not',y),exit(1))if not z==eval(y)else None
- x='!2:(0,1) +/!3:3 1+\!3:(1,2,4)'
+ x=r'!2:(0,1) +/!3:3 1+\!3:(1,2,4) (2>)(1+)\0:(0,1)'
  for _ in x.split():eq(*_.split(':'))
 def O(x):print(x,end='');sys.stdout.flush()
 if __name__=='__main__':

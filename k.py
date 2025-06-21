9919133c3bbc12bf10c56fd4c5b9469e5b3f8e44
effbiae <effbiae@gmail.com>
2025-06-21 18:22:34 +1000

    x

--- k.py
+++ k.py
@@ -2,8 +2,14 @@ from p import p,P
 def k2(x,y,z):
  if x=='+':return y+z
  if x=='>':return y>z
+ if x==',':
+  if not tt(y):y=(y,)
+  if not tt(z):z=(z,)
+  return y+z
 def k1(x,y):
  if x=='!':return tuple(range(y))
+ if x=='|':return tuple(reversed(y))
+ if x=='*':return y[0]
 def fd(x,d):
  for _ in d:
   if x in _:return _[x]
@@ -23,17 +29,23 @@ def ar(x):
   if tt(x)and x[0]!='{':return max(lar(_)for _ in x)
   if x in "xyz":return "xyz".find(x)+1
   return 1
- if type(x)==str:return (2,1)[':'in x]
- if tt(x):
-  if x[0]=='{':return lar(x[1:])
-  return 1
+ if type(x)==str and x[0]in P:return (2,1)[':'in x]
+ def car(x):return car(x[2])if tt(x)else ar(x)
+ if tt(x):return lar(x[1:]) if x[0]=='{'else car(x)if x[0]=="'"else 1
+ return 0
 def ap(x,y,d):
  if type(x)==str:return (k2,k1)[':'in x](x[0],*y)
  if tt(x):
   if x[0]=='{':return ev((('[',)+x[1:]if len(x)>2 else x[1]),(md(y),d[1:]))
+  if x[0]=="'":return ap(x[1],ap(x[2],y,d),d)if x[0]=="'"else ap(x,y,d)
   if len(x)==3 and x[2]==():return ap(x[0],(ev(x[1],d),y[0]),d)
+  if len(x)==2:assert(x[0]in"\\/'");return ad[x[0]](x[1],None,y,d)
 def oid(f):
  if f=='+':return 0
+def aca(f,y,z,d):
+ t=z;r=(t,)
+ for _ in range(ev(y,d)):t=ap(f,(t,),d);r+=(t,)
+ return r
 def acb(f,y,z,d):
  t=z;r=(t,)
  while ap(y,(t,),d):t=ap(f,(t,),d);r+=(t,)
@@ -43,14 +55,14 @@ def acc(f,y,z,d):
  if t is None and len(z):t=z[0];r=(t,);z=z[1:]
  for _ in z:t=ap(f,(t,_),d);r=r+(t,)
  return r
-def scan(x,y,z,d):return (acc,acb)[1==ar(x)](x,y,z,d)
+def scan(x,y,z,d):return(aca if nn(y) and 0==ar(y)and 1==ar(x)else(acc,acb)[1==ar(x)])(x,y,z,d)
 def over(x,y,z,d):x=scan(x,y,z,d);return x[-1]if x else x
 ad={'\\':scan,'/':over,"'":None}
 def adv(x,y,z,d):return ad[x[0]](x[1],y,z,d)
 def t():
  def eq(x,y):z=ev(p(x),({},));(print(x,'was',z,'not',y),exit(1))if not z==eval(y)else None
- x=r'!2:(0,1) +/!3:3 1+\!3:(1,2,4) (0>)(1+)/0:0 (2>)(1+)\0:(0,1,2) {2+x+y}/!2:3'
- for _ in x.split():eq(*_.split(':'))
+ x=r'!2~(0,1) +/!3~3 1+\!3~(1,2,4) (0>)(1+)/0~0 (2>)(1+)\0~(0,1,2) {2+x+y}/!2~3 *|4(+\|:)\1,1~(5,8)'
+ for _ in x.split():eq(*_.split('~'))
 def O(x):print(x,end='');sys.stdout.flush()
 if __name__=='__main__':
  t();import sys;f=sys.stdin;g={}

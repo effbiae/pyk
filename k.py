1bafc7b16ad6d656b56fa779f5f22451c7e9623f
effbiae <effbiae@gmail.com>
2025-06-21 19:04:50 +1000

    x

--- k.py
+++ k.py
@@ -53,8 +53,7 @@ def acb(f,y,z,d):
 def acc(f,y,z,d):
  r=();t=ev(y,d);t=t if nn(t)else oid(f)
  if t is None and len(z):t=z[0];r=(t,);z=z[1:]
- for _ in z:t=ap(f,(t,_),d);r=r+(t,)
- return r
+ return tuple(t:=ap(f,(t,_),d) for _ in z)
 def scan(x,y,z,d):return(aca if nn(y) and 0==ar(y)and 1==ar(x)else(acc,acb)[1==ar(x)])(x,y,z,d)
 def over(x,y,z,d):x=scan(x,y,z,d);return x[-1]if x else x
 ad={'\\':scan,'/':over,"'":None}
